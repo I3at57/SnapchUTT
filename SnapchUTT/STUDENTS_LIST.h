f3d90caabdc957f736f1d06126455dbd46f63458
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-03-19 14:51:07 +0100

    Update STUDENTS_LIST.h

--- SnapchUTT/STUDENTS_LIST.h
+++ SnapchUTT/STUDENTS_LIST.h
@@ -1,6 +1,21 @@
 #ifndef STUDENTS_LIST_H_INCLUDED
 #define STUDENTS_LIST_H_INCLUDED
 
+Student stud1 = {
+  "Baptiste TOUSSAINT",
+  19,
+  2020,
+  "Industrial Engineering",
+  "Lille",
+  {2, 3, 5},
+  NULL,
+  NULL,
+};
 
+Student tab[50] = {
+  stud1,
+  stud2,
+  stud3,
+}
 
 #endif // STUDENTS_LIST_H_INCLUDED
