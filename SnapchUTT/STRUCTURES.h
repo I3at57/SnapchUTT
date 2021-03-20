f5e96cf1cee4918b58013ee333730f0a23cebe56
Chipopotame <80981402+Chipopotame@users.noreply.github.com>
2021-03-20 15:16:56 +0100

    Update STRUCTURES.h

--- SnapchUTT/STRUCTURES.h
+++ SnapchUTT/STRUCTURES.h
@@ -1,7 +1,7 @@
 #ifndef STRUCTURES_H_INCLUDED
 #define STRUCTURES_H_INCLUDED
 
-typedef struct Student Student
+typedef struct Student Student;
 struct Student {
   /* This is the main structure of the project*/
   char name [50];
@@ -14,10 +14,10 @@ struct Student {
   Student *followStudent;
 };
 
-typedef struct LineGlossary LineGlossary
+typedef struct LineGlossary LineGlossary;
 struct LineGlossary {
   char letter;
   Student *beginList;
-}
+};
 
 #endif // STRUCTURES_H_INCLUDED
