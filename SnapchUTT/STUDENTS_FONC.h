2eebbb4f092708c294e2eb5cd6227d5074c23a98
Chipopotame <80981402+Chipopotame@users.noreply.github.com>
2021-03-20 01:27:00 +0100

    Update STUDENTS_FONC.h
    add_student, campare_strings functions for STUDENT_FUNC.h

--- SnapchUTT/STUDENTS_FONC.h
+++ SnapchUTT/STUDENTS_FONC.h
@@ -4,8 +4,63 @@
 #include "CONST.h"
 #include "STUDENTS_LIST"
 
+int compare_strings(char *str1, char *str2){
+    int k = 1;
+    while (str1[k] == str2[k] && str1[k] != '\0' && str2[k] != '\0'){
+        k++;
+    }
+    if (str1[k] < str2[k] || (str1[k] == '\0' && str2[k] != '\0')){
+        return -1;
+    } else if (str1[k] > str2[k] || (str1[k] != '\0' && str2[k] == '\0')){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int add_student(student *stud){
+    student *travelPtr;
+    char readChar = *stud.name[0];
+    int i = 0;
+    while (readChar != Glossary[i].letter && i < 27){
+        i++;
+    }
+    if (readChar == Glossary[i].letter){
+        if (Glossary[i].beginList == NULL){
+            (*stud).nextAlphaStudent = NULL;
+            Glossary[i].beginList = stud;
+            return 0;
+        } else {
+            travelPtr = Glossary[i].beginList;
+            int compare = compareStrings(*stud.name, *travelPtr.name)
+            if (compare == -1 && compare == 0){
+                (*stud).nextAlphaStudent = Glossary[i].beginList;
+                Glossary[i].beginList = stud; // necessary step because Glossary[i].beginList is not a Student element
+                return 0;
+            } else {
+                while ((*travelPtr).nextStudentAlpha != NULL){
+                    Student nextElement = (*travelPtr).nextStudentAlpha
+                    int compare = compareStrings(*stud.name, nextElement.name)
+                    if (compare == -1 && compare == 0){
+                        (*stud).nextAlphaStudent = (*travelPtr).nextStudentAlpha;
+                        (*travelPtr).nextAlphaElement = stud;
+                        return 0;
+                    } else {
+                        travelPtr = (*travelPtr).nextStudentAlpha;
+                    }
+                }
+                if ((*travelPtr).nextStudentAlpha == NULL) {
+                    (*stud).nextAlphaStudent = NULL;
+                    (*travelPtr).nextStudentAlpha = stud;
+                    return 0;
+                }
+            }
+        }
+    }
+}
+
 int init_glossary(){
-  
+
 }
 
 #endif // STUDENTS_FONC_H_INCLUDED
