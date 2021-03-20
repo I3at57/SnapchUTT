d549550d0410e1c6b54af7c5d64ca6b702b98b65
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-03-20 14:47:49 +0100

    Debog Student list

--- SnapchUTT/STUDENTS_LIST.h
+++ SnapchUTT/STUDENTS_LIST.h
@@ -2,27 +2,28 @@
 #define STUDENTS_LIST_H_INCLUDED
 
 #include "STRUCTURES.h"
+#include "CONST.h"
 
 Student stud1 = {
-  "Baptiste TOUSSAINT",
-  19,
-  2020,
-  "Industrial Engineering",
-  "Lille",
-  {2, 3, 5},
-  NULL,
-  NULL,
+    "Baptiste TOUSSAINT",
+    19,
+    2020,
+    "Industrial Engineering",
+    "Lille",
+    NULL,
+    NULL,
+    {interst2, interst3, interst5}
 };
 
 Student stud2 = {
-  "Lucas MARTINEZ",
-  19,
-  2020,
-  "Mecanical Engineering",
-  "Troyes",
-  {5, 8, 1},
-  NULL,
-  NULL,
+    "Lucas MARTINEZ",
+    19,
+    2020,
+    "Mecanical Engineering",
+    "Troyes",
+    NULL,
+    NULL,
+    {interst5, interst8, interst1}
 };
 
 int nbrStudent = 2;
@@ -30,6 +31,6 @@ int nbrStudent = 2;
 Student *tab[2] = {
   &stud1,
   &stud2,
-}
+};
 
 #endif // STUDENTS_LIST_H_INCLUDED
