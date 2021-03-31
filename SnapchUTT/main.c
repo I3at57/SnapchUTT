2820d3f32442c4030da703193dc3e393cd67b47f
Chipopotame <80981402+Chipopotame@users.noreply.github.com>
2021-03-31 18:15:39 +0200

    Update main.c

--- SnapchUTT/main.c
+++ SnapchUTT/main.c
@@ -1,10 +1,11 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "FUNCTIONS.h"
 
 int main()
 {
     /*Put every student of STUDENTS_LIST in glossary*/
     init_glossary(stuTab, nbrStudent);
     main_menu();    //The main menu of the application
-
     return(0);
 }
