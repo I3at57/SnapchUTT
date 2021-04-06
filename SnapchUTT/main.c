3032520f45be48a4264bf2ecf35bf138643e63b0
Chipopotame <80981402+Chipopotame@users.noreply.github.com>
2021-04-06 20:19:45 +0200

    Update main.c

--- SnapchUTT/main.c
+++ SnapchUTT/main.c
@@ -6,6 +6,7 @@ int main()
 {
     /*Put every student of STUDENTS_LIST in glossary*/
     init_glossary();
-    main_menu();    //The main menu of the application
+    main_menu();//The main menu of the application
+    quit();
     return(0);
 }
