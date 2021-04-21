b85a72ba774ce1bb796c3cc2fa6d58244b913576
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-04-21 21:22:45 +0200

    Commentary

--- Snapchutt/main.c
+++ Snapchutt/main.c
@@ -8,7 +8,7 @@ int main()
     /*Put every student of STUDENTS_LIST in glossary*/
     init_glossary();
     //init_followers();
-    main_menu();//The main menu of the application
+    main_menu();//The main menu of the project
     quit();
     return(0);
 }
